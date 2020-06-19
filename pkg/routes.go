4db41276bbfee3ff2cf45cc3e19897bbf3373e31
Maneesh Babu M <maneeshbabum@gmail.com>
2020-06-19 18:07:49 +0000

    Remove dummy routes

--- pkg/routes.go
+++ pkg/routes.go
@@ -1,9 +1,6 @@
 package pkg
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/labstack/echo"
 )
 
@@ -12,36 +9,5 @@ type Server struct {
 }
 
 func (s *Server) Mount(e *echo.Echo) {
-	e.GET("/hello", s.GetHello)
-	e.POST("/hello", s.PostHello)
-	e.GET("/greeting", s.GetGreeting)
-	e.POST("/address", s.PostAddress)
-}
-
-func (s *Server) GetHello(c echo.Context) error {
-	log.Print(c.Request())
-
-	return c.JSON(http.StatusOK, "Hello World")
-}
-
-func (s *Server) PostHello(c echo.Context) error {
-	log.Print(c.Request())
-	log.Print(c.ParamValues())
-	log.Print(c.FormParams())
-
-	name := c.FormValue("name")
-	hello := "Hello, " + name + "!"
-
-	return c.JSON(http.StatusOK, hello)
-}
-
-func (s *Server) GetGreeting(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Welcome to test")
-}
-
-func (s *Server) PostAddress(c echo.Context) error {
-	address := c.FormValue("address")
-	txt := "Your address is " + address
-
-	return c.JSON(http.StatusOK, txt)
+	e.Static("/", "frontend/dist/")
 }
